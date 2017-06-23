d7288e1314de0322b1beff72b9c39687b6c6b0d8
jfpires <jerssonpires@gmail.com>
2017-06-23 13:48:05 +0200

    made div for all ingredients

--- dev/ingredients.ts
+++ dev/ingredients.ts
@@ -13,7 +13,7 @@ class Ingredients{
 
     }
 
-    // creates div for all ingredients
+    // this creates div for all ingredients
     protected createDiv(ingredient, x, y, key){
         document.body.appendChild(ingredient).innerHTML=key;
         ingredient.style.transform ="translate("+x+"px,"+y+"px)";   
